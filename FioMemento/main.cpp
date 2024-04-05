9d5ffdde0eec9a2e93abd403639c006d33980f7e
Sergiy Stoyan <s.y.stoyan@gmail.com>
2024-04-05 21:14:06 +0300

    no boost

--- FioMemento/main.cpp
+++ FioMemento/main.cpp
@@ -10,8 +10,6 @@
 #include <string.h>
 #include <chrono>
 
-//#include <appbase/application.hpp>
-
 //extern "C" {
 //#include "cJSON.h"
 //}
@@ -42,23 +40,5 @@ int main(int argc, char** argv)
 		STDOUT_CURRENT_EXCEPTION(NULL);
 	}
 
-	/*try {
-		appbase::app().register_plugin<receiver_plugin>();	
-		if (!appbase::app().initialize<receiver_plugin>(argc, argv))
-			return -1;
-		initialize_logging();
-		appbase::app().startup();
-		appbase::app().exec();
-	}
-	catch (const boost::exception& e) {
-		std::cerr << boost::diagnostic_information(e) << "\n";
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << "\n";
-	}
-	catch (...) {
-		std::cerr << "unknown exception\n";
-	}
-	std::cout << "exited cleanly\n";
-	return 0;*/
+
 }

3d2470ca4fcf58eab5df8a42fe8f07a7f70be210
misterleet <michael@fioprotocol.io>
2024-11-05 09:58:13 -0700

    Update uninstall-pgsql.sh

--- scripts/uninstall-pgsql.sh
+++ scripts/uninstall-pgsql.sh
@@ -15,6 +15,10 @@ function pause(){
   echo
 }
 
+echo
+echo "Confirm complete removal of the PostgreSQL packages and all related artifacts..."
+pause
+
 apt-get --purge remove postgresql
 apt-get --purge remove postgresql-17
 apt-get --purge remove postgresql-16
@@ -22,22 +26,32 @@ apt-get --purge remove postgresql-16
 echo
 echo "Checking for any remaining PostgreSQL packages..."
 dpkg -l | grep postgres
-
 echo
-echo "Run the following command to remove each package listed above; sudo apt-get --purge remove <package>"
-pause
+for pkg in `dpkg -l | grep postgres | awk '{print $2}'`; do
+  echo Removing $pkg...
+  pause
+  echo
+  apt-get --purge remove ${pkg}
+done
 
 echo
 echo "Removing PostgreSQL related directories..."
+pause
 rm -rf /var/lib/postgresql/
 rm -rf /var/log/postgresql/
 rm -rf /etc/postgresql/
 
 echo
 echo "Removing postgres user..."
+pause
 deluser postgres
 
 echo
 echo "Verifying PostgreSQL has been removed; the following command should fail..."
 echo
-psql --version
+psql --version &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "WARNING: PostgreSQL has NOT been fully removed from the system; manual intervention is required to do so! Exiting..."
+  exit 1
+fi
+echo "PostgreSQL has been successfully removed from the system"

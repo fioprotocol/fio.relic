d04cb2490f2de3a07b83628fdd5de81709c3a637
Ed Rotthoff <ed@fioprotocol.io>
2025-02-24 21:43:49 +0000

    fixes for table triggers burn domain and address

--- scripts/sql/CreateUpdDomainBurnt.sql
+++ scripts/sql/CreateUpdDomainBurnt.sql
@@ -1,28 +1,17 @@
 CREATE OR REPLACE FUNCTION upddomainburnt( 
-    domainname    varchar(62),
-    expirationtimestamp timestamp,
-    ispublic boolean 
+    domainname    varchar(62)
 ) RETURNS int 
 LANGUAGE 'plpgsql'
 COST 100 
 VOLATILE AS 
 $BODY$  
-    declare pkid bigint;
-    declare domexpire timestamp;
-    declare domispublic boolean;
     BEGIN  
-    SELECT pk_domain_id, expiration_timestamp, is_public INTO 
-    pkid, domexpire, domispublic FROM domains
-         WHERE domain_name = domainname; 
-    
-    IF(domexpire = expirationtimestamp AND domexpire = ispublic) THEN
+   
+
         UPDATE  domains SET 
                 domain_status = 'burnt'
-            WHERE pk_domain_id = pkid;
+            WHERE domain_name = domainname;
         RETURN 1;
-    ELSE 
-        RETURN 0;
-    END IF;
 
     END;
 $BODY$;
